e80c8396df9b6088fdb3985205e1991f3c500d6e
Tombo-x <motoki.o.biz@gmail.com>
2025-09-11 06:44:18 +0900

    init commit

--- components.py
+++ components.py
@@ -169,7 +169,7 @@ def display_search_llm_response(llm_response):
         # ページ番号が取得できた場合のみ、ページ番号を表示（ドキュメントによっては取得できない場合がある）
         if "page" in llm_response["context"][0].metadata:
             # ページ番号を取得
-            main_page_number = llm_response["context"][0].metadata["page"]
+            main_page_number = llm_response["context"][0].metadata["page"]+1
             # 「メインドキュメントのファイルパス」と「ページ番号」を表示
             st.success(f"{main_file_path}(ページNo. {main_page_number})", icon=icon)
         else:
@@ -204,7 +204,7 @@ def display_search_llm_response(llm_response):
             # ページ番号が取得できない場合のための分岐処理
             if "page" in document.metadata:
                 # ページ番号を取得
-                sub_page_number = document.metadata["page"]
+                sub_page_number = document.metadata["page"]+1
                 # 「サブドキュメントのファイルパス」と「ページ番号」の辞書を作成
                 sub_choice = {"source": sub_file_path, "page_number": sub_page_number}
             else:
@@ -305,7 +305,7 @@ def display_contact_llm_response(llm_response):
             # ページ番号が取得できた場合のみ、ページ番号を表示（ドキュメントによっては取得できない場合がある）
             if "page" in document.metadata:
                 # ページ番号を取得
-                page_number = document.metadata["page"]
+                page_number = document.metadata["page"]+1
                 # 「ファイルパス」と「ページ番号」
                 file_info = f"{file_path}(ページNo.{page_number})"
             else:
